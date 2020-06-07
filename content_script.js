1b04346558cae277cb4cbb360cbcd155240f5e12
AlexandreLeonetti <al.leo@wanadoo.fr>
2020-06-07 23:58:56 +0800

    up

---
+++ content_script.js
@@ -0,0 +1,16 @@
+let style = document.createElement('style');
+	document.body.appendChild(style);
+	browser.storage.onChanged.addListener((changes, area) => {
+		if (area === 'local' && 'value' in changes){
+			update(changes.value.newValue);
+		}
+	});
+
+function update(value) {
+	style.innerText = `html{ filter:sepia(${value}%) !important}`;
+}
+
+
+browser.storage.local.get('value').then(result => update(result.value));
+
+
---
+++ popup.js
@@ -0,0 +1,22 @@
+let input = document.querySelector('input');
+
+input.addEventListener('change', e=> setValue(e.target.value));
+
+async function setValue(value) {
+	await browser.storage.local.set({value});
+}
+
+
+
+async function init() {
+	let {value} = browser.local.storage.get('value');
+	if(!value){
+		value=0;
+	}
+	input.value=value;
+	setValue(value);
+}
+
+
+init().catch(e => console.error(e));
+
